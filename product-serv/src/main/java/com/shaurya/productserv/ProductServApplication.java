40d64b67baf100c374f016f829bfa571b0ac068f
antidoteShaurya <shaurya.7.chamoli@gmail.com>
2022-03-13 17:49:04 +0530

    first commit for product-serv

---
+++ product-serv/src/main/java/com/shaurya/productserv/ProductServApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.productserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServApplication.class, args);
+	}
+
+}
---
+++ product-serv/src/test/java/com/shaurya/productserv/ProductServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.productserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
